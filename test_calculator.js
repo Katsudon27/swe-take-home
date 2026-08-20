ab0aa65556e7dfabe14824ac64b33159ff7402a2
Katsudon27 <152286098+Katsudon27@users.noreply.github.com>
2026-08-20 10:38:11 +1000

    test: add tests for getHEM

--- test_calculator.js
+++ test_calculator.js
@@ -4,7 +4,7 @@
 
 
 const assert = require('assert'); 
-const {calculateBorrowingPower, getTax} = require('./borrowingCalculator');
+const {calculateBorrowingPower, getTax, getHEM} = require('./borrowingCalculator');
 
 describe('Term Deposit Calculator Tests', () => {
 
@@ -31,5 +31,18 @@ describe('getTax Tests', () => {
   it('should return 0 for 0 income', async () => {
     const result = await getTax(0);
     assert.strictEqual(result, 0);
+  });
+});
+
+describe('getHEM Tests', () => {
+
+  it('should return the calculated HEM monthly baseline cost', async () => {
+    const result = await getHEM(125000, 2);
+    assert.strictEqual(result, 3100);
+  });
+
+  it('should return 1600 for 0 income and 0 dependent', async () => {
+    const result = await getHEM(0, 0);
+    assert.strictEqual(result, 1600);
   });
 });
